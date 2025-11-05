c9b1ef684daac049eebb2e54cfb1a11741a524e1
Amit Singh <h4775990@gmail.com>
2025-11-05 18:42:05 +0530

    update

--- ecommerce/shop/urls/payment.py
+++ ecommerce/shop/urls/payment.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from ..views.payment import PaymentProcessor
+from ..views.webhook import StripeWebhookView
 
 urlpatterns = [
     path(
         "payment/",
+        PaymentProcessor.as_view(),
+        name="register_user",
+    ),
+    path(
+        "webhook/",
         PaymentProcessor.as_view(),
         name="register_user",
     ),
